558da7687950c1a10b838ba1f42f9250aa88e636
Matthew Gelbman <mgelbman@planningcenter.local>
2018-07-30 10:09:01 -0700

    fix month + days

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ var b = moment();
 var m = b.diff(a, 'months');
 var w = b.diff(a, 'weeks');
 var d = b.diff(a, 'days');
+var curr = moment().date();
 
-var prevMoBday = b.subtract(1, 'months').startOf('month').add(26, 'days').format('YYYY-MM-DD');
-var mDays = moment().diff(prevMoBday, 'days');
-console.log(mDays);
+if (curr < 27) {
+    var prevMoBday = b.subtract(1, 'months').startOf('month').add(26, 'days').format('YYYY-MM-DD');
+    var mDays = moment().diff(prevMoBday, 'days');
+} else if (curr > 27) {
+    var dDiff = curr - 27;
+    var mDays = " and " + dDiff + " days ";
+} else if (curr = 27){
+    var mDays =  '';
+}
+
+console.log(dDiff);
+
 
 var wPlus = Math.round(d - (w * 7));
 var mPlus = m;
 
-document.getElementById("demo").innerHTML = "Quincy is " + m + " months and " + mDays +" days!<br>"+"Quincy is " + w + " weeks and " + wPlus +" days old today!<br>Quincy is " + "<strong>" + d +"</strong>" + " days old today!";
+document.getElementById("demo").innerHTML = 
+
+"Quincy is " + m + " months" + mDays +" old today!<br>" +
+
+"Quincy is " + w + " weeks and " + wPlus + " days old today!<br> Quincy is " + "<strong>" + d +"</strong>" + " days old today!";
