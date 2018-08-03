f11d33c4615f330a4c509ef0ccbf4b90e8616d45
Matthew Gelbman <mgelbman@planningcenter.local>
2018-08-03 07:51:19 -0700

    update

--- script.js
+++ script.js
@@ -7,22 +7,20 @@ var curr = moment().date();
 
 if (curr < 27) {
     var prevMoBday = b.subtract(1, 'months').startOf('month').add(26, 'days').format('YYYY-MM-DD');
-    var mDays = moment().diff(prevMoBday, 'days');
+    var dDiff = moment().diff(prevMoBday, 'days');
+    var mDays = " and " + dDiff + " days ";
 } else if (curr > 27) {
     var dDiff = curr - 27;
     var mDays = " and " + dDiff + " days ";
-} else if (curr = 27){
-    var mDays =  '';
+} else if (curr = 27) {
+    var mDays = '';
 }
-
-console.log(dDiff);
-
 
 var wPlus = Math.round(d - (w * 7));
 var mPlus = m;
 
-document.getElementById("demo").innerHTML = 
+document.getElementById("demo").innerHTML =
 
-"Quincy is " + m + " months" + mDays +" old today!<br>" +
+    "Quincy is " + m + " months" + mDays + " old today!<br>" +
 
-"Quincy is " + w + " weeks and " + wPlus + " days old today!<br> Quincy is " + "<strong>" + d +"</strong>" + " days old today!";
+    "Quincy is " + w + " weeks and " + wPlus + " days old today!<br> Quincy is " + "<strong>" + d + "</strong>" + " days old today!";
