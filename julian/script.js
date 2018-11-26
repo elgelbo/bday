94f5ea9a1d1bcde4fad67ddc58d84d8af563c8e5
el_gelbo <mgelbman@gmail.com>
2018-11-26 13:13:32 -0800

    update j&j

--- julian/script.js
+++ julian/script.js
@@ -1,26 +1,81 @@
-var a = moment([2018, 1, 16]);
-var b = moment();
-var m = b.diff(a, 'months');
-var w = b.diff(a, 'weeks');
-var d = b.diff(a, 'days');
-var curr = moment().date();
+const name = 'Julian'
+const a = moment([2018, 1, 16]);
+const now = moment();
+const y = now.diff(a, 'years', true);
+const m = now.diff(a, 'months', true);
+const w = now.diff(a, 'weeks', true);
+const d = now.diff(a, 'days', true);
+const curr = now.date();
+const prevMoBday = now.subtract(1, 'months').startOf('month').add((a.date() - 1), 'days');
 
-if (curr < 16) {
-    var prevMoBday = b.subtract(1, 'months').startOf('month').add(15, 'days').format('YYYY-MM-DD');
-    var dDiff = moment().diff(prevMoBday, 'days');
-    var mDays = " and " + dDiff + " days ";
-} else if (curr > 16) {
-    var dDiff = curr - 16;
-    var mDays = " and " + dDiff + " days ";
-} else if (curr = 16) {
-    var mDays = '';
+
+
+let age = '';
+
+age += `${name} is`
+
+// DISPLAY YEARS
+if (y > 2) {
+    age += ' ' + parseInt(y) + ' years'
+} else if (y < 2 && y >= 1) {
+    age += ' ' + parseInt(y) + ' year';
+}
+const m2 = parseInt(m) - (parseInt(y) * 12);  
+
+// TODO: fix months for less than a year
+if (m > ((parseInt(y) * 12) + 1)) {
+    if (y >= 1) {
+        age += ', '
+    } else {
+        age += ' '
+    }
+    if (m2 >= 1 && m2 < 2) {
+        age += m2 + ' month';
+    } else if (m2 >= 2) {
+        age += m2 + ' months';
+    }
 }
 
-var wPlus = Math.round(d - (w * 7));
-var mPlus = m;
+getWeeks = (days) => {
+    var weeks = parseInt(days/7);
+    var dAdj = days - (weeks*7);
+    let wkDy = '';
+    if (days === 1) {
+        wkDy += ` and ${days} day`
+    } else if (days < 7) {
+        wkDy += ` and ${days} days`
+    } else if (days >= 7 && days < 14) {
+        wkDy += `, ${weeks} week`
+        if (dAdj === 1) {
+            wkDy += `, and ${dAdj} day`
+        } else if (dAdj > 1) {
+            wkDy += `, and ${dAdj} days`
+        }
+    } else if (days > 14) {
+        wkDy += `, ${weeks} weeks`
+        if (dAdj === 1) {
+            wkDy += `, and ${dAdj} day`
+        } else if (dAdj > 1) {
+            wkDy += `, and ${dAdj} days`
+        }
+    }
 
-document.getElementById("demo").innerHTML =
+    return wkDy;
+}
 
-    "Julian is " + m + " months" + mDays + " old today!<br>" +
+if (curr < a.date()){
+    var dDiff = moment().diff(prevMoBday, 'days');
+    var weeks = getWeeks(dDiff);
+    age += weeks;
+} else if (curr > a.date()){
+    var dDiff = curr - a.date();  
+    var weeks = getWeeks(dDiff);
+    age += weeks;
+}
 
-    "Julian is " + w + " weeks and " + wPlus + " days old today! <br> Julian is " + "<strong>" + d + "</strong>" + " days old today!";
+age += ' old today!'
+
+document.getElementById("demo").innerHTML = age;
+document.getElementById("detail-days").innerHTML = `${parseInt(d)} days |`;
+document.getElementById("detail-weeks").innerHTML = `${parseInt(w)} weeks |`;
+document.getElementById("detail-months").innerHTML = `${parseInt(m)} months`;
